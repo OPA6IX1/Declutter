44fa94c292be5b9780ec2e4d9d37aa78c6849bd9
OPAKUNLE SODIQ <oluwaferanmiopakunle94@gmail.com>
2025-01-24 20:31:44 +0100

    pictures and installations

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+       <h1 className="text-3xl bg-blue-600 w-[50%] flex justify-center font-bold underline">
+          Hello world!
+        </h1>
+    </div>
+  )
+}
+
+export default App
