2244876cad0f0de5c2ee2d43b5495eb3c906cc3c
OPAKUNLE SODIQ <oluwaferanmiopakunle94@gmail.com>
2025-01-30 18:39:50 +0100

    conflict settle

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,8 @@ import Footer from "./components/footer"
 
 
 // import Home from "./pages/home";
-// import LogIn from "./pages/logIn";
-// import SignUp from "./pages/signUp";
+import LogIn from "./pages/logIn";
+import SignUp from "./pages/signUp";
 import Contact from "./pages/contact";
 
 const App = () => {
@@ -16,30 +16,20 @@ const App = () => {
     
        
           <div className="h-16 w-full flex justify-center  fixed top-0 left-0 bg-black shadow-lg z-50">
-<<<<<<< HEAD
-            <div className="flex flex-col md:flex-row items-center justify-between gap-4 px-4 md:px-8 py-2 ">
-              <p className="text-[14px] md:text-[16px] font-[poppins] font-[400] text-white text-center opacity-70">Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!</p>
-              
-              <div className="flex justify-center mt-8 gap-70 ">
-                <p className="text-[18px]  md:text-[16px] font-[poppins] font[600] text-white">
-=======
+
             <div className="flex flex-col md:flex-row items-center justify-between gap-4 px-4 md:px-8 py-2">
               <p className="text-[18px] md:text-[18px] font-[poppins] font-[400] text-white text-center opacity-70 ">Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!</p>
               
               <div className="flex justify-center mt-7 gap-70 ">
                 <p className="text-[20px] md:text-[18px] font-[poppins] font[600] text-white">
->>>>>>> Opa6ix
                   <a href="/login" className="text-white underline">
                     ShopNow
                   </a>
                 </p>
-<<<<<<< HEAD
+
                 <p className="flex mb-8 text-[30px] md:text-[16px] font-[poppins] font[600] text-white ">
                 English<RiArrowDropDownLine size={30}/>
-=======
-                <p className="flex mb-8  text-[16px] md:text-[18px] font-[poppins] font[400] text-white ">
-                English<RiArrowDropDownLine size={20}/>
->>>>>>> Opa6ix
+
                 </p>
               </div>
             </div>
@@ -51,21 +41,19 @@ const App = () => {
 
     <main className="mt-16">
          <Routes>
-        {/* <Route path="/" element={<LogIn />} /> */}
-        {/* <Route path="logIn" element={<LogIn />} /> */}
-        {/* <Route path="SignUp" element={<SignUp />} /> */}
+        <Route path="/" element={<LogIn />} />
+        <Route path="logIn" element={<LogIn />} />
+        <Route path="SignUp" element={<SignUp />} />
         <Route path="contact" element={<Contact />} />
         </Routes>
     </main>
 
-<<<<<<< HEAD
+
       
      <div className="mt-15  w-full">
         <Footer/>
       </div>  
-=======
-       <Footer/>  
->>>>>>> Opa6ix
+
   </>
 
   )
--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -7,20 +7,7 @@ import { NavLink } from "react-router-dom";
 
 const navBar = () => {
   return (
-<<<<<<< HEAD
-    <nav className="w-full flex items-center justify-evenly mt-5 py-7 pb-10 fixed top-11 left-0 z-50 text-black border border-slate-300">
-    <h1 className="text-[25px] md:text-[20] font-bold font-[inter]">Exclusive</h1>
 
-    <ul className=" text-[20px] md:text-[16px] font-[poppins] font-[400] hover:text-gray-300 hidden lg:flex gap-8 items-center text-lg">
-      <li><NavLink to="/" className="">Home</NavLink></li>
-      <li><NavLink to="/contact" className="">Contact</NavLink></li>
-      <li><NavLink to="/about" className="">About</NavLink></li>
-      <li><NavLink to="/signup" className="">Sign Up</NavLink></li>
-    </ul>
-    
-        <div className="flex gap-5 items-center ">
-            <form className="flex items-center gap-2 border-none bg-gray-200 px-4 py-2 rounded-md ">
-=======
     <nav className="w-full flex items-center justify-evenly mt-5 pb-5 fixed top-11 left-0 z-50 text-black border border-slate-300">
     <h1 className="text-2xl md:text-[20] font-bold font-[inter]">Exclusive</h1>
 
@@ -33,22 +20,15 @@ const navBar = () => {
     
     <div className="flex gap-5 my-7 items-center ">
         <form className="flex items-center gap-2 border-none bg-gray-100 px-4 py-2 rounded-md">
->>>>>>> Opa6ix
           
              <input
               type="text"
               placeholder="What are you looking for?"
-<<<<<<< HEAD
-              className="bg-transparent outline-none text-customGray font- font-light"/>
-         <IoSearchOutline size={15}/>
-        </form>
 
-=======
               className="bg-transparent outline-none border-none text-[18px] md:text-[15px] font-[poppins] font-medium"
          />
          <IoSearchOutline size={20} />
         </form>
->>>>>>> Opa6ix
         <div>
           <CiHeart size={20} />
         </div>
@@ -58,14 +38,10 @@ const navBar = () => {
         </div>
 
         <div className=" bg-[#DB4444] rounded-full p-1 text-white">
-<<<<<<< HEAD
-          <FiUser size={13} />
-        </div>
-=======
+
           <FiUser size={15} />
         </div>
       
->>>>>>> Opa6ix
         </div>
 
   </nav>
